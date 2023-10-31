c273ec86403841cae782a100ce28b87d47a24d35
Makarios Vardaxis <salonikamakarios@gmail.com>
2023-10-31 20:36:57 +0200

    Place forntend url as string on the email view files

--- resources/views/emails/resetPasswordEmail.blade.php
+++ resources/views/emails/resetPasswordEmail.blade.php
@@ -6,8 +6,8 @@
     <body>
         <h1>Καλώς ήρθατε ξανά στην εφαρμογή μας!</h1>
         <p>Για να επαληθεύσετε το email σας πατήστε στον σύνδεσμο:</p>
-        <p><a href="{{ env('FRONT_END_URL').'/passwordSet?token='.$user->reset_password_token }}">Επαναφωρά Κωδικού</a></p>
-        <p>Ή εδώ: <a href="{{ env('FRONT_END_URL').'/passwordSet?token='.$user->reset_password_token }}">{{ env('FRONT_END_URL').'/passwordSet?token='.$user->reset_password_token }}</a></p>
+        <p><a href="{{ 'https://antallakseto.gr/passwordSet?token='.$user->reset_password_token }}">Επαναφωρά Κωδικού</a></p>
+        <p>Ή εδώ: <a href="{{ 'https://antallakseto.gr/passwordSet?token='.$user->reset_password_token }}">{{ env('FRONT_END_URL').'/passwordSet?token='.$user->reset_password_token }}</a></p>
         <p>Ευχόμαστε να απολαύσετε την εμπειρία σας με την εφαρμογή μας. Εάν έχετε οποιεσδήποτε ερωτήσεις ή ανάγκη από βοήθεια, παρακαλούμε επικοινωνήστε μαζί μας στο antallaxeto.gr@gmail.com.</p>
     </body>
 </html>

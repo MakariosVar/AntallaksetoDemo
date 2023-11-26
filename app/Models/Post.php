ccab20b464dec49e787af6d59949d0396b049ec5
Makarios Vardaxis <salonikamakarios@gmail.com>
2023-11-26 02:02:09 +0200

    Extend the Voyager's models.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends \TCG\Voyager\Models\Post
 {
    protected $guarded = [];
 
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class Role extends \TCG\Voyager\Models\Role
 {
     // ...
 }
